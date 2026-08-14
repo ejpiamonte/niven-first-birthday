f2ebd16826ccd9e2a1a7c9104869243bad8f24eb
EJ Piamonte <ejpiamonte21@gmail.com>
2026-08-15 02:49:14 +0800

    test completed

--- src/components/Invitation.tsx
+++ src/components/Invitation.tsx
@@ -70,7 +70,7 @@ export default function Invitation() {
                 transition={{ duration: 1 }}
                 className="text-xs uppercase tracking-[0.4em] text-[#D8B76A]"
               >
-                A Special Invitations
+                A Special Invitation
               </motion.p>
 
               <motion.div
